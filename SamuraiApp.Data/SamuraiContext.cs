cdc76477e29f95d4865d4d20c3badb88aa266914
Gabriiell <gabriel.opereyra@gmail.com>
2019-11-04 01:11:33 -0300

    Add battle date types

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -14,16 +14,27 @@ namespace SamuraiApp.Data
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
+        //public static readonly ILoggerFactory SamuraiLoggerFactory
+        //    = new LoggerFactory(new[] {
+        //          new ConsoleLoggerProvider((category, level) =>
+        //            category == DbLoggerCategory.Database.Command.Name &&
+        //            level == LogLevel.Information, true)
+        //        });
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             var connection = "Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True; ";
-            options.UseSqlServer(connection);
+            options
+                //.UseLoggerFactory(SamuraiLoggerFactory)
+                .UseSqlServer(connection)
+                .EnableSensitiveDataLogging(true);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<SamuraiBattle>()
-                .HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Battle>().Property(b => b.StartDate).HasColumnType("Date");
+            modelBuilder.Entity<Battle>().Property(b => b.EndDate).HasColumnType("Date");
         }
     }
 }
